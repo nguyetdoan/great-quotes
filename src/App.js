6332815967ecfb752c18be0f23ecf6fba69dd667
nguyetdoan <minhnguyetdoan057@gmail.com>
2021-10-04 22:12:36 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import { Fragment } from "react";
-import Counter from "./components/Counter";
-import Header from "./components/Header";
-import Auth from "./components/Auth";
-import UserProfile from "./components/UserProfile";
-import { useSelector } from "react-redux";
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
   return (
-    <Fragment>
-      <Header />
-      {!isAuthenticated && <Auth /> }
-      {isAuthenticated && <UserProfile /> }
-      <Counter />
-    </Fragment>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
 export default App;
+
