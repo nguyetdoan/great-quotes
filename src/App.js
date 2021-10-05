fef904c5235eec836be0bd2df7d8382aa0e8a794
nguyetdoan <minhnguyetdoan057@gmail.com>
2021-10-05 23:48:50 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import { Fragment, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import Notification from './components/UI/Notification';
-import { sendCartData, fetchCartData } from './store/cart-action';
-
 function App() {
-  const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
-  const cart = useSelector(state => state.cart);
-  const dispatch = useDispatch()
-  const notification = useSelector(state => state.ui.notification);
-  
-  useEffect(() => {
-    dispatch(fetchCartData());
-  }, [dispatch])
-
-  useEffect(() => {
-    if (cart.changed) {
-      dispatch(sendCartData(cart));
-    }
-  }, [cart, dispatch]);
-
   return (
-    <Fragment>
-      {notification && <Notification {...notification}/>}
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
-    </Fragment>
+    <div>
+
+    </div>
   );
 }
 
 export default App;
-
